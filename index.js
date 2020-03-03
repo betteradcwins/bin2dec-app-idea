91466c280da1aa36ae80bc4d264442148ef881a4
Breno H. Oliveira <brenohenriqueof@gmail.com>
2020-03-02 23:36:57 -0300

    Added: implemented the logic for converting binary to decimal and decimal to binary.

---
+++ index.js
@@ -0,0 +1,36 @@
+const hasAlert = () => document.querySelector('.alert')
+
+const createAlert = message => {
+    if (!hasAlert()) {
+        const div = document.createElement('div')
+        div.className = 'alert'
+        div.innerHTML = message
+        document.querySelector('.container')
+        .appendChild(div)
+    }
+}
+
+function convert(id, regex, callback, message, value) {
+    if (hasAlert()) document.querySelector('.container').removeChild(hasAlert())
+    
+    const num = document.getElementById(id)
+    if (value.match(regex)) {
+        const newValue = callback()
+        num.value = newValue
+    } else if(value === '') {
+        num.value = ''
+    } else {
+        createAlert(message)
+    }
+}
+
+const convertBin2Dec = (value) => 
+    convert('decimal', /^[0-1]+$/g, 
+    () => parseInt(value, 2), 
+    'Please, only enter 0 or 1.', value)
+
+const convertDec2Bin = (value) => 
+    convert('binary', /^[0-9]+$/g,
+    () => parseInt(value, 10).toString(2),
+    'Please, only enter numbers between 0 and 9.', value)
+
