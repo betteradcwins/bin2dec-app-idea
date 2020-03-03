3488c5c1fd9ea1bb54e264f75e9d026c4d08c74c
Breno H. Oliveira <brenohenriqueof@gmail.com>
2020-03-03 01:55:59 -0300

    Added: hexadecimal converter.

--- index.js
+++ index.js
@@ -10,38 +10,58 @@ const createAlert = message => {
     }
 }
 
-function convert(id, regex, callback, message, value) {
+function convert(ids, regex, callback, message, value) {
     if (hasAlert()) document.querySelector('.container').removeChild(hasAlert())
     
-    const num = document.getElementById(id)
-    if (value.match(regex)) {
-        const newValue = callback()
-        num.value = newValue
-    } else if(value === '') {
-        num.value = ''
-    } else {
-        createAlert(message)
-    }
+    ids.forEach((id, i) => {
+        const num = document.getElementById(id)
+        if (value.match(regex)) {
+            const newValue = callback()[i]
+            num.value = newValue.toString()
+        } else if(value === '') {
+            num.value = ''
+        } else {
+            createAlert(message)
+        }
+    })
 }
 
-const convertBin2Dec = (value) => 
-    convert('decimal', /^[0-1]+$/g, 
-    () => parseInt(value, 2), 
+const convertBin2DecAndHex = (value) => 
+    convert(['decimal','hexadecimal'],
+    /^[0-1]+$/g, 
+    () => [parseInt(value, 2),
+        parseInt(value, 2).toString(16)], 
     'Please, only enter 0 or 1.', value)
 
-const convertDec2Bin = (value) => 
-    convert('binary', /^[0-9]+$/g,
-    () => parseInt(value, 10).toString(2),
+const convertDec2BinAndHex = (value) => 
+    convert(['binary','hexadecimal'],
+    /^[0-9]+$/g,
+    () => [parseInt(value, 10).toString(2),
+        parseInt(value, 10).toString(16)],
     'Please, only enter numbers.', value)
 
+const convertHex2BinAndDec = (value) => 
+    convert(['binary','decimal'], 
+    /^([0-9]|[A-F]|[a-f])+$/g,
+    () => [parseInt(value, 16).toString(2), 
+        parseInt(value, 16).toString(10),],
+    'Please, only enter numbers or letters between A to F.',
+     value)
+
+
 function changeTitle(elem) {
-    const strong1 = document.querySelector('[strong-1]')
-    const strong2 = document.querySelector('[strong-2]')
+    const strongs = document.querySelectorAll('strong')
     if (elem.id === 'binary') {
-        strong1.innerHTML = 'Binary'
-        strong2.innerHTML = 'decimal'
+        strongs[0].innerHTML = 'Binary'
+        strongs[1].innerHTML = 'decimal'
+        strongs[2].innerHTML = 'hexadecimal'
+    } else if (elem.id === 'decimal'){
+        strongs[0].innerHTML = 'Decimal'
+        strongs[1].innerHTML = 'binary'
+        strongs[2].innerHTML = 'hexadecimal'
     } else {
-        strong2.innerHTML = 'binary'
-        strong1.innerHTML = 'Decimal'
+        strongs[0].innerHTML = 'Hexadecimal'
+        strongs[1].innerHTML = 'binary'
+        strongs[2].innerHTML = 'decimal'
     }
 }
